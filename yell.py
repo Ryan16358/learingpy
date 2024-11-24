d19a417dbec34ec12042898c8070e4436529e904
Ryan16358 <mdrayhanh585@gmail.com>
2024-11-24 14:35:07 +0600

    Initial commit

---
+++ yell.py
@@ -0,0 +1,13 @@
+def main():
+    yell("this", "is", "cs50")
+
+
+
+
+def yell(*words):
+    uppercased = [word.upper() for word in words]
+    print(*uppercased)
+
+
+if __name__ == "__main__":
+    main()
