8ea3eb397a278dd59ea1bcfb939f17d9aa5abd3a
Ryan16358 <mdrayhanh585@gmail.com>
2025-04-04 14:03:07 +0600

    this simplewebsite finished for know

--- python/file.py
+++
@@ -1,4 +0,0 @@
-name = input("what's your name ?")
-
-with open("names.txt", "a") as f:
-    f.write(f"{name}\n")
--- simplewebsite/index.js
+++ simplewebsite/index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const port = 8080;
 const { v4: uuidv4 } = require("uuid");
-uuidv4();
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(methodOverride("_method"));
 let posts = [
   { id: uuidv4(), username: "john", content: "hello world" },
 
@@ -39,6 +39,26 @@ app.get("/posts/:id", (req, res) => {
   let { id } = req.params;
   let post = posts.find((p) => id === p.id);
   res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => id === p.id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id !== p.id);
+  res.redirect("/posts");
 });
 
 app.listen(port, () => {
